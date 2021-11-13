101706e1a152450ccd7081dd0a81698d842061b8
raz0red <raz0red@gmail.com>
2021-11-12 17:43:02 -0800

    Updated drop on text to work on Mac

--- src/components/common/editor/EditorTextField.js
+++ src/components/common/editor/EditorTextField.js
@@ -6,25 +6,33 @@ import {
 
 
 export default function ItemTextField(props) {
-  const { 
-    sx, 
+  const {
+    sx,
     onDropText,
-    ...other 
+    ...other
   } = props;
 
   return (
-    <TextField    
+    <TextField
       onDrop={(e) => {
         if (onDropText) {
           e.preventDefault();
-          const text = e.dataTransfer.getData("text")
-          if (isValidString(text)) {
-            onDropText(text);
+
+          for (let i = 0; i < e.dataTransfer.items.length; i++) {
+            const item = e.dataTransfer.items[i];
+            if (item.kind === 'string' &&
+              (item.type.match('^text/uri-list') || item.type.match('^text/plain'))) {
+              item.getAsString((text) => {
+                if (isValidString(text)) {
+                  onDropText(text);
+                }
+              });
+            }
           }
         }
-      }} 
+      }}
       sx={{
-        m: 1.5, 
+        m: 1.5,
         width: '35ch',
         ...sx
       }}
